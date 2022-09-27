a1ed92588ab4362e00704449b837171bcf100393
Guilherme Gualberto <guilherme.moreira1997@hotmail.com>
2022-09-27 14:02:38 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/Person.java
+++ src/main/java/com/educandoweb/course/entities/Person.java
@@ -2,21 +2,29 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 
-public class User implements Serializable{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
 	
-	public User() {
+	public Person() {
 		
 	}
 
-	public User(Long id, String name, String email, String phone, String password) {
+	public Person(Long id, String name, String email, String phone, String password) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -80,7 +88,7 @@ public class User implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		User other = (User) obj;
+		Person other = (Person) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
--- src/main/java/com/educandoweb/course/resources/PersonResource.java
+++ src/main/java/com/educandoweb/course/resources/PersonResource.java
@@ -1,19 +1,20 @@
 package com.educandoweb.course.resources;
 
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.Person;
 
 @RestController
 @RequestMapping(value = "/users")
-public class UserResource {
+public class PersonResource {
 	
 	@GetMapping
-	public ResponseEntity<User> findALL(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "11980526523", "123456");
+	public ResponseEntity<Person> findALL(){
+		Person u = new Person(1L, "Maria", "maria@gmail.com", "11980526523", "123456");
 		return ResponseEntity.ok().body(u);
 		
 	}
